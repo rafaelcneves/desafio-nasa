e2b4125f2f58bb3d489b3b86104384dba0e7bb24
Rafael César Neves <rafael.ceesar@gmail.com>
2017-01-29 16:26:52 -0200

    initial commit with maven

---
+++ src/main/java/com/rafael/desafio/App.java
@@ -0,0 +1,19 @@
+package com.rafael.desafio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        // char commands[];
+
+        // for (char commands : command) {
+            
+        // }
+    }
+}
