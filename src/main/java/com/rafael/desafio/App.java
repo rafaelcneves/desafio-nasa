b43539ddf8760284a3dc28364795f0f36b59e1ab
Rafael César Neves <rafael.neves@humantech.com.br>
2017-01-31 11:44:45 -0200

    Some changes

--- src/main/java/com/rafael/desafio/App.java
+++ src/main/java/com/rafael/desafio/App.java
@@ -12,33 +12,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class App
 {
-    // public static void main( String[] args )
-    // {
-    //     Nasa nasa = new Nasa("MMRMMRMM");
-    //     try {
-    //         nasa.execute();
-    //         nasa.printPosition();
-    //     } catch (Exception e) {
-    //         System.out.println(e.getMessage());
-    //     }
-    // }
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
-    // @Bean
-    // public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    //     return args -> {
-
-    //         System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-    //         String[] beanNames = ctx.getBeanDefinitionNames();
-    //         Arrays.sort(beanNames);
-    //         for (String beanName : beanNames) {
-    //             System.out.println(beanName);
-    //         }
-
-    //     };
-    // }
 }
